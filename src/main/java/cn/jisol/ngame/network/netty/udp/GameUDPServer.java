7bf7a2131fb8c5d532bc21ae9d2d23d0288f51e0
JisolKun <2858626794@qq.com>
2021-11-30 16:32:54 +0800

    更新DEMO

--- src/main/java/cn/jisol/ngame/network/netty/udp/GameUDPServer.java
+++ src/main/java/cn/jisol/ngame/network/netty/udp/GameUDPServer.java
@@ -1,6 +1,7 @@
 package cn.jisol.ngame.network.netty.udp;
 
 
+import cn.jisol.ngame.client.nclient.UDPClient;
 import cn.jisol.ngame.netty.annotation.AJNetty;
 import cn.jisol.ngame.netty.annotation.control.JNClose;
 import cn.jisol.ngame.netty.annotation.control.JNInit;
@@ -12,6 +13,12 @@ import cn.jisol.ngame.netty.network.udp.session.UDPSessionGroup;
 import cn.jisol.ngame.network.netty.udp.decoders.DefaultProtoBufDecoder;
 import cn.jisol.ngame.network.netty.udp.encoders.DefaultProtoBufEncoder;
 import cn.jisol.ngame.proto.message.NGameMessageOuterClass.*;
+import cn.jisol.ngame.room.defalut.DefaultNRoom;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 启动Netty UDP
@@ -28,6 +35,8 @@ import cn.jisol.ngame.proto.message.NGameMessageOuterClass.*;
 )
 public class GameUDPServer {
 
+    public static final Map<String, UDPClient> CLIENTS = new ConcurrentHashMap<>();
+
     @JNInit
     public void initNetwork(UDPJNettyNetwork network){
         System.out.println("initNetwork");
@@ -40,17 +49,25 @@ public class GameUDPServer {
 
     @JNOpen
     public void onOpen(UDPSession session,UDPSessionGroup clients){
+        //将session添加到Clients中
+        CLIENTS.put(session.getSid(),new UDPClient(session));
         System.out.println(String.format("【%s】连接服务器 - 当前服务器在线人数:%s",session.getSid(),clients.size()));
     }
 
     @JNMessage
-    public void onMessage(UDPSession session, NGameMessage message,String text){
+    public void onMessage(UDPSession session,UDPSessionGroup clients, NGameMessage message,String text){
+        //查询clients中的client
+        UDPClient client = null;
+        if(Objects.isNull(client = CLIENTS.get(session.getSid()))) this.onOpen(session,clients);
+
         System.out.println(String.format("【%s】接收到消息UID:%s",session.getSid(),message.getUid()));
         session.vSendMessage(message);
     }
 
     @JNClose
     public void onClose(UDPSession session,UDPSessionGroup clients){
+        //移除Client
+        CLIENTS.remove(session.getSid());
         System.out.println(String.format("【%s】离开服务器 - 当前服务器在线人数:%s",session.getSid(),clients.size()));
     }
 }
