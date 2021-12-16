17227983ebef01c5c3868b3d36be25ae187654e2
Jisol_Kyu <2858626794@qq.com>
2021-12-16 08:52:14 +0000

    删除文件 src

--- src/test/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-//        AnyArray.Builder anyArray = AnyArray.newBuilder();
-//
-//        anyArray.addMessage(0, Any.pack(MSyncFPSInfo.NAction.newBuilder().build()));
-//
-//        System.out.println(anyArray.build().toByteArray()[0]);;
-    }
-}
